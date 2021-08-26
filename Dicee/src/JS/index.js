c71cad3e789a63760ab61d3fec09dd67b787a825
Rubén Escribano Martín <rubenescribanomartin@gmail.com>
2021-08-26 20:56:42 +0200

    Dicee PreRelease

--- Dicee/src/JS/index.js
+++ Dicee/src/JS/index.js
@@ -1,3 +1,6 @@
+var player1Number = 0;
+var player2Number = 0;
+
 function rollDice() {
   var n = Math.random();
   n = Math.floor(n * 6) + 1;
@@ -5,7 +8,7 @@ function rollDice() {
 }
 
 function lanzarDado1() {
-  var player1Number = rollDice();
+  player1Number = rollDice();
   if (player1Number === 1) {
     document.querySelector("#dice1").setAttribute("src", "src/Images/Dices/Dice1.png");
   } else if (player1Number === 2) {
@@ -19,26 +22,35 @@ function lanzarDado1() {
   } else {
     document.querySelector("#dice1").setAttribute("src", "src/Images/Dices/Dice6.png");
   }
+  return player1Number;
 }
 
 function lanzarDado2() {
-  var player1Number = rollDice();
-  if (player1Number === 1) {
+  player2Number = rollDice();
+  if (player2Number === 1) {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice1.png");
-  } else if (player1Number === 2) {
+  } else if (player2Number === 2) {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice2.png");
-  } else if (player1Number === 3) {
+  } else if (player2Number === 3) {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice3.png");
-  } else if (player1Number === 4) {
+  } else if (player2Number === 4) {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice4.png");
-  } else if (player1Number === 5) {
+  } else if (player2Number === 5) {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice5.png");
   } else {
     document.querySelector("#dice2").setAttribute("src", "src/Images/Dices/Dice6.png");
   }
+  return player2Number;
 }
 
 function lanzarDados() {
   lanzarDado1();
   lanzarDado2();
+  if (player1Number > player2Number) {
+    document.querySelector("h1").innerHTML = "<strong>Player 1 Wins!</strong>";
+  } else if (player1Number === player2Number) {
+    document.querySelector("h1").innerHTML = "<strong>It's a draw!</strong>";
+  } else {
+    document.querySelector("h1").innerHTML = "<strong>Player 2 Wins!</strong>";
+  }
 }
